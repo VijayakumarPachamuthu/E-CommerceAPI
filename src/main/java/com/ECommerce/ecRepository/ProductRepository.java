670da32844f4c702bf562cf58c786fe007805a89
“VijayakumarPachamuthu” <pvijay.dce@gmail.com>
2025-07-28 12:12:26 +0530

    Filter Changes

--- src/main/java/com/ECommerce/ecRepository/ProductRepository.java
+++ src/main/java/com/ECommerce/ecRepository/ProductRepository.java
@@ -11,7 +11,8 @@ import feign.Param;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer>  {
 	
-	@Query("SELECT p FROM ProductEntity p WHERE p.brand = :brand")
+//	@Query("SELECT p FROM ProductEntity p WHERE p.brand = :brand")
+	@Query("SELECT p FROM ProductEntity p WHERE LOWER(p.brand) LIKE LOWER(CONCAT('%', :brand, '%'))")
 	List<ProductEntity> getBrandName(@Param("brand") String brand);
 
 
